b71c023be141b5ad59ad4ce0614235c931daabf8
kamil.rybacki <kamil.rybacki@pg.edu.pl>
2022-05-30 13:39:35 +0200

    changed check to async

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,75 +1,74 @@
+/* eslint-disable require-jsdoc */
 import {Component} from 'solid-js';
-import {BiCompass} from "solid-icons/bi";
+import {BiCompass} from 'solid-icons/bi';
 
 import {render} from 'solid-testing-library';
 
-import stw from '../src/index'; 
+import stw from '../src/index';
 
-async function getElementWithIdAfterRender (renderFunction: Function, id: string) {
-    await renderFunction()
-    console.info(document.body.innerHTML)
-    return document.getElementById(id);
+async function getElementWithIdAfterRender(renderFunction: Function, id: string) {
+  await renderFunction();
+  console.info(document.body.innerHTML);
+  return document.getElementById(id);
 }
 
-function styleAndContentsCheck (renderFunction: Function, id: string) {
-    return getElementWithIdAfterRender(renderFunction, id)
-    .then((spawnedElement: HTMLElement) => {
-        const appliedClasses = [...spawnedElement.classList.values()];
-        expect(appliedClasses).toContain('h-10');
-        expect(appliedClasses).toContain('w-8');
-
-        const areChildrenAppended = spawnedElement.hasChildNodes();
-        expect(areChildrenAppended).toBeTruthy();
-    });
+async function styleAndContentsCheck(renderFunction: Function, id: string) {
+  const spawnedElement = await getElementWithIdAfterRender(renderFunction, id);
+  const appliedClasses = [...spawnedElement.classList.values()];
+  expect(appliedClasses).toContain('h-10');
+  expect(appliedClasses).toContain('w-8');
+  const areChildrenAppended = spawnedElement.hasChildNodes();
+  expect(areChildrenAppended).toBeTruthy();
 }
 
 test('Styling semantic HTML element', () => {
-    const StyledDiv = stw('div')`
+  const StyledDiv = stw('div')`
         h-10
         w-8
     `;
 
-    const testElementId = 'test_div';
+  const testElementId = 'test_div';
 
-    return styleAndContentsCheck(() => {
-        render(
-            <StyledDiv id={testElementId}>
+  return styleAndContentsCheck(() => {
+    render(
+        <StyledDiv id={testElementId}>
                 Test content
-            </StyledDiv>
-        )
-    }, testElementId);
+        </StyledDiv>,
+    );
+  }, testElementId);
 });
 
 test('Styling SolidJS component', () => {
-    const UnstyledSolidJSComponent: Component<string> = (title: string, ...props) => {
-        return <h1 {...props}>{title}</h1>;
-    } 
+  const UnstyledSolidJSComponent: Component<string> = (title: string, ...props) => {
+    // @ts-ignore
+    return <h1 {...props}>{title}</h1>;
+  };
 
-    const StyledSolidJSComponent = stw(UnstyledSolidJSComponent)`
+  const StyledSolidJSComponent = stw(UnstyledSolidJSComponent)`
         h-10
         w-8
     `;
 
-    const testElementId = 'test_div';
+  const testElementId = 'test_div';
 
-    return styleAndContentsCheck(() => {
-        render(
-            <StyledSolidJSComponent id={testElementId}>
+  return styleAndContentsCheck(() => {
+    render(
+        <StyledSolidJSComponent id={testElementId}>
                 Test content
-            </StyledSolidJSComponent>
-        )
-    }, testElementId);
-})
+        </StyledSolidJSComponent>,
+    );
+  }, testElementId);
+});
 
 test('Styling external library component', () => {
-    const StyledIcon = stw(BiCompass)`
+  const StyledIcon = stw(BiCompass)`
         h-10
         w-8
     `;
 
-    const testElementId = 'test_div';
+  const testElementId = 'test_div';
 
-    return styleAndContentsCheck(() => {
-        render(<StyledIcon id={testElementId}/>)
-    }, testElementId);
+  return styleAndContentsCheck(() => {
+    render(<StyledIcon id={testElementId}/>);
+  }, testElementId);
 });
